0b7ad07ba2c7172430a987440a58a9eea114540a
Srinivaslancius <srinivas@lanciussolutions.com>
2017-10-31 10:44:49 +0530

    remove unwanted files

---
+++ admin_includes/email_sms_templates_components.php
@@ -0,0 +1,20 @@
+<?php
+    
+    //common function for SMS and Email Templates
+
+    function sendMobileSMS($message,$user_mobile) {
+        global $conn;
+        $username = "*****";
+        $password = "*****";
+        $numbers = "$user_mobile"; // mobile number
+        $sender = urlencode('*****'); // assigned Sender_ID
+        // Message text required to deliver on mobile number
+        $data = "username="."$username"."&password="."$password"."&to="."$numbers"."&from="."$sender"."&msg="."$message"."&type=1";
+        $data = "https://www.smsstriker.com/API/sms.php?".$data;
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$data);
+        $response = curl_exec($ch);        
+    }
+
+    
+?>
