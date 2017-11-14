a8c3a4ab6a16625a88d1b7fc35673a9ce65dd78c
Gunavardhan <vardhan.gujjula@gmail.com>
2017-11-14 19:11:36 +0530

    side navigation committed

--- services_manage_webmaster/admin_includes/side_navigation.php
+++ services_manage_webmaster/admin_includes/side_navigation.php
@@ -114,7 +114,7 @@
                 <span class="menu-icon">
                    <i class="zmdi zmdi-collection-image zmdi-hc-fw"></i>
                 </span>
-                <span class="menu-text">New Letters</span>
+                <span class="menu-text">News Letters</span>
               </a>
             </li>
             <li  class="<?php if($page_name == 'services_our_branches.php' || $page_name == 'add_services_our_branches.php' || $page_name == 'edit_services_our_branches.php' ) { echo "active"; } ?>">
