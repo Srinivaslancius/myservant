9911bc4f9cf73a2100006c05ee047988550423be
srinivaslancius <srinivas@lanciussolutions.com>
2017-11-16 17:44:46 +0530

    Changed top header and added logout page

--- Services/top_header.php
+++ Services/top_header.php
@@ -1,7 +1,25 @@
-<?php include_once('../admin_includes/config.php');
-	include_once('../admin_includes/common_functions.php');
-	$getSiteSettings = getAllDataWhere('services_site_settings','id','1'); 
-    $getSiteSettingsData = $getSiteSettings->fetch_assoc();
+<?php 
+error_reporting(1);
+include_once('../admin_includes/config.php');
+include_once('../admin_includes/common_functions.php');
+$getSiteSettings = getAllDataWhere('services_site_settings','id','1'); 
+$getSiteSettingsData = $getSiteSettings->fetch_assoc();
+if(isset($_POST['sign_in']))  { 
+    //Login here
+    $user_email = $_POST['login_email'];
+    $user_password = encryptPassword($_POST['login_password']);
+    $sql = "SELECT * FROM users WHERE user_email = '$user_email' AND user_password = '$user_password' AND lkp_status_id = 1 ";
+    $result = $conn->query($sql);
+    //Set variable for session
+    if($row = $result->fetch_assoc()) {
+        $_SESSION['user_login_session_id'] =  $row['id'];
+        $_SESSION['user_login_session_name'] = $row['user_full_name'];
+        $_SESSION['user_login_session_email'] = $row['user_email'];
+        header('Location: index.php');
+    } else {
+    	echo "<script>alert('invalid username/password.  Please try again');window.location='index.php';</script>";
+    }
+}
 ?>
 <div id="top_line">
 			<div class="container">
@@ -12,9 +30,12 @@
 					<div class="col-md-6 col-sm-6 col-xs-6">
 						<ul id="top_links">
 							<li>
-								<div class="dropdown dropdown-access">
-									<a href="#" class="dropdown-toggle" data-toggle="dropdown" id="access_link">Sign in</a>
-									<div class="dropdown-menu">
+							<div class="dropdown dropdown-access">
+								<?php if(isset($_SESSION['user_login_session_id']) && $_SESSION['user_login_session_id']!='') { ?>
+									<a href="index.php"><?php echo $_SESSION['user_login_session_name']; ?> </a> &nbsp;|&nbsp;<a href="logout.php"> Logout </a>
+								<?php } else { ?>
+					                <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="access_link">Sign in</a>
+					                <div class="dropdown-menu">
 										<div class="row">
 											<div class="col-md-6 col-sm-6 col-xs-6">
 												<a href="#" class="bt_facebook">
@@ -29,20 +50,24 @@
 											<hr class="hr-or">
 											<span class="span-or">or</span>
 										</div>
+										<form action="" method="post">
 										<div class="form-group">
-											<input type="text" class="form-control" id="inputUsernameEmail" placeholder="Email">
+											<input type="email" name="login_email" class="form-control" id="inputUsernameEmail" placeholder="Email" required>
 										</div>
 										<div class="form-group">
-											<input type="password" class="form-control" id="inputPassword" placeholder="Password">
+											<input type="password" name="login_password" class="form-control" id="inputPassword" placeholder="Password"required>
 										</div>
 										<a id="forgot_pw" href="#">Forgot password?</a>
-										<input type="submit" name="Sign_in" value="Sign in" id="Sign_in" class="button_drop">
+										<input type="submit" name="sign_in" value="Sign in" id="Sign_in" class="button_drop">
 										<input type="submit" name="Sign_up" value="Sign up" id="Sign_up" class="button_drop outline">
+										</form>
 									</div>
+					        </li>
+                            <li><a href="#" id="wishlist_link">Register</a></li>
+										<?php } ?>
 								</div>
 								<!-- End Dropdown access -->
-							</li>
-                                                        <li><a href="#" id="wishlist_link">Register</a></li>
+							
                                                         
 						</ul>
 					</div>
