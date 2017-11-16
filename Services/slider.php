91d44c7aec613bf426a5c97f993e03eb8f16b24c
Gunavardhan <vardhan.gujjula@gmail.com>
2017-11-16 19:17:06 +0530

    slider changes committed

--- Services/slider.php
+++ Services/slider.php
@@ -1,23 +1,22 @@
 <?php 
-	$getQry = getAllDataWithStatus('services_category','0');
-	while($getCatId = $getQry->fetch_assoc()){
+	$getServiceCatData = getAllDataWithStatus('services_category','0');
+	while($getCatId = $getServiceCatData->fetch_assoc()){
 		$catId = $getCatId['id'];
 	}	
 ?>
 		<div id="layerslider" style="width:100%;height:600px;">
 				<!-- first slide -->
 				<?php 
-					$sql = "SELECT * FROM services_banners WHERE service_category_id = '$catId' OR service_category_id = 0 AND lkp_status_id = 0 ";
-					$getBanners = $conn->query($sql);
-				while($getBannerData = $getBanners->fetch_assoc()){?>
+					$getBanners = "SELECT * FROM services_banners WHERE service_category_id = '$catId' OR service_category_id = 0 AND lkp_status_id = 0 ";
+					$getBannersData = $conn->query($getBanners);
+				while($getBannersAllData = $getBannersData->fetch_assoc()){?>
 				<div class="ls-slide" data-ls="slidedelay: 5000; transition2d:5;">
-					<img src="<?php echo $base_url . 'uploads/services_banner_images/'.$getBannerData['banner'] ?>" class="ls-bg" alt="Slide background">					
-					<h3 class="ls-l slide_typo" style="top: 45%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;"><?php echo $getBannerData['title'];?></h3>
-					<p class="ls-l slide_typo_2" style="top:52%; left:50%;" data-ls="durationin:2000;delayin:1000;easingin:easeOutElastic;">Sample Text Replace with original text</p>
+					<img src="<?php echo $base_url . 'uploads/services_banner_images/'.$getBannersAllData['banner'] ?>" class="ls-bg" alt="Slide background">					
+					<h3 class="ls-l slide_typo" style="top: 45%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;"><?php echo $getBannersAllData['title'];?></h3>
 					<?php 
-						$getCatId = $getBannerData['service_category_id'];
+						$getCatId = $getBannersAllData['service_category_id'];
 							if(!empty($getCatId)) { ?>
-							<a class="ls-l button_intro_2 outline" style="top:370px; left:50%;white-space: nowrap;" data-ls="durationin:2000;delayin:1400;easingin:easeOutElastic;" href='all_tour_list.php?cid=<?php echo $getBannerData['service_category_id']?>'>Read more</a>
+							<a class="ls-l button_intro_2 outline" style="top:370px; left:50%;white-space: nowrap;" data-ls="durationin:2000;delayin:1400;easingin:easeOutElastic;" href='all_tour_list.php?cid=<?php echo $getBannersAllData['service_category_id']?>'>Read more</a>
 						<?php }?>	
 				</div>
 				<?php }?>
