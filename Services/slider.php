07092d7bcc8a5fd488a924d3132966147526adeb
Srinivaslancius <srinivas@lanciussolutions.com>
2017-11-16 19:26:19 +0530

    banner issue

--- Services/slider.php
+++ Services/slider.php
@@ -1,27 +1,16 @@
-<?php 
-	$getQry = "SELECT * FROM services_category WHERE lkp_status_id = 0";			
-	$excQry = $conn->query($getQry);
-	while($getCatId = $excQry->fetch_assoc()){
-		$catId = $getCatId['id'];
-	}	
+<div id="layerslider" style="width:100%;height:600px;">
+<?php $banners = "SELECT * FROM `services_banners` AS sb, `services_category` sc WHERE sc.`lkp_status_id` = '0' AND  sb.`lkp_status_id` = '0 ' "; 
+	$getBanners = $conn->query($banners);      
 ?>
-		<div id="layerslider" style="width:100%;height:600px;">
-				<!-- first slide -->
-				<?php 
-					$sql = "SELECT * FROM services_banners WHERE service_category_id = '$catId' AND lkp_status_id = 0 ";
-					$getBanners = $conn->query($sql);
-				while($getBannerData = $getBanners->fetch_assoc()){?>
-				<div class="ls-slide" data-ls="slidedelay: 5000; transition2d:5;">
-					<img src="<?php echo $base_url . 'uploads/services_banner_images/'.$getBannerData['banner'] ?>" class="ls-bg" alt="Slide background">					
-					<h3 class="ls-l slide_typo" style="top: 45%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;"><?php echo $getBannerData['title'];?></h3>
-					<p class="ls-l slide_typo_2" style="top:52%; left:50%;" data-ls="durationin:2000;delayin:1000;easingin:easeOutElastic;">Sample Text Replace with original text</p>
-					<?php 
-						$getCatId = $getBannerData['service_category_id'];
-							if(!empty($getCatId)) { ?>
-							<a class="ls-l button_intro_2 outline" style="top:370px; left:50%;white-space: nowrap;" data-ls="durationin:2000;delayin:1400;easingin:easeOutElastic;" href='all_tour_list.php?cid=<?php echo $getBannerData['service_category_id']?>'>Read more</a>
-						<?php }?>	
-				</div>
-				<?php }?>
-				<!-- second slide -->
-			</div>
-			
+	<!-- first slide -->
+	<?php while($getBannerData = $getBanners->fetch_assoc()) { ?>
+	<div class="ls-slide" data-ls="slidedelay: 3000; transition2d:5;">
+		<img src="<?php echo $base_url . 'uploads/services_banner_images/'.$getBannerData['banner'] ?>" class="ls-bg" alt="<?php echo $getBannerData['title'];?>"><h3 class="ls-l slide_typo" style="top: 45%; left: 50%;" data-ls="offsetxin:0;durationin:2000;delayin:1000;easingin:easeOutElastic;rotatexin:90;transformoriginin:50% bottom 0;offsetxout:0;rotatexout:90;transformoriginout:50% bottom 0;"><?php echo $getBannerData['title'];?></h3>
+		<!-- <p class="ls-l slide_typo_2" style="top:52%; left:50%;" data-ls="durationin:2000;delayin:1000;easingin:easeOutElastic;">Sample Text Replace with original text</p> -->
+		<?php if($getBannerData['service_category_id']!=0) { ?>
+			<a class="ls-l button_intro_2 outline" style="top:370px; left:50%;white-space: nowrap;" data-ls="durationin:2000;delayin:1400;easingin:easeOutElastic;" href='all_tour_list.php?cid=<?php echo $getBannerData['service_category_id']?>'>Read more</a>
+		<?php } ?>
+	</div>
+	<?php } ?>
+
+</div>
