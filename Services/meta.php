9deae0f47c93a2f194c0548bf93b531f71a2c189
Srinivaslancius <srinivas@lanciussolutions.com>
2017-11-16 12:10:00 +0530

    new chanegs

--- Services/meta.php
+++ Services/meta.php
@@ -1,3 +1,9 @@
+<?php
+ob_start();
+include "../admin_includes/config.php";
+include "../admin_includes/common_functions.php"; 
+
+?>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<meta name="description" content="">
