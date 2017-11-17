4587823662424018373146a2a54edc1d71722954
Srinivaslancius <srinivas@lanciussolutions.com>
2017-11-17 16:50:29 +0530

    brands display changes

--- Services/brands.php
+++ Services/brands.php
@@ -2,177 +2,19 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans|Bitter" rel="stylesheet" type="text/css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-<style>
-.carousel-showmanymoveone
-{
-  .carousel-control
-  { 
-    width: 4%;
-    background-image:none;
 
-    &.left 
-    {
-      margin-left:15px;
-    }
-
-    &.right 
-    {
-      margin-right:15px;
-    }
-  }
-
-  .cloneditem-1, 
-  .cloneditem-2, 
-  .cloneditem-3
-  {
-    display: none;
-  }
-
-  .carousel-inner
-  {
-    @media all and (min-width: 768px)
-    {
-      @media (transform-3d), (-webkit-transform-3d)
-      {
-        > .item.active.right,
-        > .item.next
-        { 
-          transform: translate3d(50%, 0, 0);  
-          left: 0;
-        }
-
-        > .item.active.left,
-        > .item.prev
-        { 
-          transform: translate3d(-50%, 0, 0);
-          left: 0;
-        }
-
-        > .item.left,
-        > .item.prev.right,
-        > .item.active
-        {
-          transform: translate3d(0, 0, 0);
-          left: 0;
-        }    
-      } 
-
-      > .active.left,
-      > .prev
-      {
-        left: -50%;
-      }
-
-      > .active.right,
-      > .next
-      {
-        left:  50%;
-      }
-
-      > .left,
-      > .prev.right,
-      > .active
-      {
-        left: 0;
-      }
-
-      .cloneditem-1 
-      {
-        display: block;
-      }
-    }
-
-    @media all and (min-width: 992px)
-    {    
-      @media (transform-3d), (-webkit-transform-3d)
-      {
-        > .item.active.right,
-        > .item.next
-        { 
-          transform: translate3d(25%, 0, 0);  
-          left: 0;
-        }    
-
-        > .item.active.left,
-        > .item.prev
-        { 
-          transform: translate3d(-25%, 0, 0);
-          left: 0;
-        }
-
-        > .item.left,
-        > .item.prev.right,
-        > .item.active
-        {
-          transform: translate3d(0, 0, 0);
-          left: 0;
-        }
-      }
-
-      > .active.left,
-      > .prev
-      {
-        left: -25%;
-      }
-
-      > .active.right,
-      > .next
-      {
-        left:  25%;
-      }
-
-      > .left,
-      > .prev.right,
-      > .active
-      {
-        left: 0;
-      }
-
-      .cloneditem-2, 
-      .cloneditem-3
-      {
-        display: block;
-      }
-    }    
-  }
-}
-
-.logo
-{
-  margin: 20px auto;
-  height: 100px;  
-}
-  @media only screen and (min-width : 768px) {  
-    margin: 28px;
-    float: left;
-  }
-}
-</style>
-</head>
 <div class="container">
   <div class="row">
     <div class="col-md-12">
       <div class="carousel carousel-showmanymoveone slide" id="carousel123">
         <div class="carousel-inner">
-          <div class="item active">
-            <div class="col-xs-12 col-sm-2 col-md-2"><a href=""><img src="img/guide_1.jpg"></a></div>
-          </div>
-          <div class="item">
-             <div class="col-xs-12 col-sm-2 col-md-2"><a href=""><img src="img/guide_1.jpg"></a></div>
-          </div>
-          <div class="item">
-          <div class="col-xs-12 col-sm-2 col-md-2"><a href=""><img src="img/guide_1.jpg"></a></div>
-          </div>          
-          <div class="item">
-          <div class="col-xs-12 col-sm-2 col-md-2"><a href=""><img src="img/guide_1.jpg"></a></div>
-          </div>
-          <div class="item">
-         <div class="col-xs-12 col-sm-2 col-md-2"><a href=""><img src="img/guide_1.jpg"></a></div>
-          </div>
-          <div class="item">
-        <div class="col-xs-12 col-sm-2 col-md-2"><a href=""><img src="img/guide_1.jpg"></a></div>
-          </div>
-         
+
+          <?php $getBrands = getAllDataWithStatus('services_brand_logos','0'); ?>
+          <?php while($getAllBrands = $getBrands->fetch_assoc()) { ?> 
+            <div class="item <?php if($getAllBrands['id']==4) { echo "active"; } ?>">
+              <div class="col-xs-12 col-sm-2 col-md-2"><a href=""><img src="<?php echo $base_url . 'uploads/services_brand_logos/'.$getAllBrands['brand_logo'] ?>" class="img-responsive" alt="<?php echo $getAllBrands['title']; ?>"></a></div>
+            </div>
+           <?php } ?>      
         </div>
         <a class="left carousel-control" href="#carousel123" data-slide="prev"><i class="glyphicon glyphicon-chevron-left" style="margin-left:-90px;color:#f26226"></i></a>
         <a class="right carousel-control" href="#carousel123" data-slide="next"><i class="glyphicon glyphicon-chevron-right"style="margin-right:-90px;color:#f26226"></i></a>
@@ -181,32 +23,4 @@
   </div> 
 </div>
 
-<script>
-(function(){
-  // setup your carousels as you normally would using JS
-  // or via data attributes according to the documentation
-  // https://getbootstrap.com/javascript/#carousel
-  $('#carousel123').carousel({ interval: 2000 });
-  $('#carouselABC').carousel({ interval: 3600 });
-}());
 
-(function(){
-  $('.carousel-showmanymoveone .item').each(function(){
-    var itemToClone = $(this);
-
-    for (var i=1;i<6;i++) {
-      itemToClone = itemToClone.next();
-
-      // wrap around if at end of item collection
-      if (!itemToClone.length) {
-        itemToClone = $(this).siblings(':first');
-      }
-
-      // grab item, clone, add marker class, add to collection
-      itemToClone.children(':first-child').clone()
-        .addClass("cloneditem-"+(i))
-        .appendTo($(this));
-    }
-  });
-}());
-</script>
--- Services/index.php
+++ Services/index.php
@@ -200,6 +200,35 @@
 			});
 		});
 	</script>
+	<script>
+(function(){
+  // setup your carousels as you normally would using JS
+  // or via data attributes according to the documentation
+  // https://getbootstrap.com/javascript/#carousel
+  $('#carousel123').carousel({ interval: 2000 });
+  $('#carouselABC').carousel({ interval: 3600 });
+}());
+
+(function(){
+  $('.carousel-showmanymoveone .item').each(function(){
+    var itemToClone = $(this);
+
+    for (var i=1;i<6;i++) {
+      itemToClone = itemToClone.next();
+
+      // wrap around if at end of item collection
+      if (!itemToClone.length) {
+        itemToClone = $(this).siblings(':first');
+      }
+
+      // grab item, clone, add marker class, add to collection
+      itemToClone.children(':first-child').clone()
+        .addClass("cloneditem-"+(i))
+        .appendTo($(this));
+    }
+  });
+}());
+</script>
 
 </body>
 
