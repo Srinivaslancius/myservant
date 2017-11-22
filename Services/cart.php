ff89504d184cf3fe47fd2442cd848f096f0452fc
Srinivaslancius <srinivas@lanciussolutions.com>
2017-11-22 20:05:09 +0530

    new desing cjhnages

--- Services/cart.php
+++ Services/cart.php
@@ -129,11 +129,12 @@
 							</th>
 						</tr>
 					</thead>
+					<form name="cart_form" method="post" action="update_cart.php">
 					<tbody>
 						<?php $cartTotal = 0;  
                               while ($getCartItems = $cartItems->fetch_assoc()) { 
-                              
                         ?>
+                         <input type="hidden" name="cart_id[]" value="<?php echo $getCartItems['id']; ?>">
 						<tr>
 						<?php $getSerName= getIndividualDetails('services_group_service_names','id',$getCartItems['service_id']); ?>
                         <td><?php echo $getSerName['group_service_name']; ?></td>
@@ -146,8 +147,8 @@
                         <?php } else { ?>
                             <td><?php echo $getSerName['service_min_price']; ?> - <?php echo $getSerName['service_max_price']; ?></td>
                         <?php } ?>
-                        <td><input class="date-pick form-control" data-date-format="M d, D" type="text"></td>
-                        <td><input class="time-pick form-control" value="12:00 AM" type="text"></td>
+                        <td><input class="date-pick form-control" type="text" name="service_visit_date[]"></td>
+                        <td><input class="time-pick form-control" type="text" name="service_visit_time[]"></td>
                         <!-- <td>
                             <div class="numbers-row">
                                 <input type="text" value="1" id="quantity_2" class="qty2 form-control" name="quantity_2">
@@ -174,7 +175,7 @@
 						</div>
 					</div>
 					<div class="pull-right fix_mobile">
-						<button type="button" class="btn_cart_outine">Update Cart</button>
+						<input type="submit" class="btn_cart_outine" value="Update Cart" name="submit">
 					</div>
 				</div>
 				<div class="row">
@@ -188,6 +189,7 @@
                         <a href="checkout.php" class="btn_full">Proceed to Checkout <i class="icon-left"></i></a>
 					</div>
 				</div>
+				</form>
 				<?php }  else { ?>
         			<p style="text-align:center; color:#e04f67">No Services In Your Cart</p>
         		<?php } ?>
