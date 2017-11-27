9f92db2c730559727e3acd7f3a6a6de794297603
Srinivaslancius <srinivas@lanciussolutions.com>
2017-11-27 19:05:08 +0530

    banner ikanges

--- Services/save_cart.php
+++ Services/save_cart.php
@@ -7,6 +7,10 @@ if($_SESSION['CART_TEMP_RANDOM'] == "") {
 
 	$_SESSION['CART_TEMP_RANDOM'] = rand(10, 10).sha1(crypt(time())).time();
 }
+if($_SESSION['user_login_session_id'] != "") {
+
+	$_SESSION['user_login_session_id'] = 0;
+}
 $user_id = $_SESSION['user_login_session_id'];
 $session_cart_id = $_SESSION['CART_TEMP_RANDOM'];
 $category_id = decryptPassword($_POST['services_cat_id']);
@@ -17,15 +21,15 @@ $services_service_id = $_POST['services_service_id'];
 $service_price_type_id = $_POST['service_price_type_id'];
 $created_at = date("Y-m-d h:i:s");
 
-$checkCartItems = "SELECT * FROM services_cart WHERE category_id ='$category_id' AND sub_cat_id='$sub_cat_id' AND group_id = '$group_id' AND  service_id='$services_service_id' AND session_cart_id ='$session_cart_id' ";
+$checkCartItems = "SELECT * FROM services_cart WHERE group_id = '$group_id' AND service_id='$services_service_id' AND session_cart_id ='$session_cart_id' ";
 $getCartCount = $conn->query($checkCartItems);
 $getTotalCount = $getCartCount->num_rows;
 if($getTotalCount > 0) {
 	echo $getTotalCount;
 } else {
-	$saveItems = "INSERT INTO `services_cart`(`user_id`, `session_cart_id`, `category_id`, `sub_cat_id`, `group_id`, `service_id`, `services_price_type_id`, `service_price`, `created_at`) VALUES ('$user_id','$session_cart_id','$category_id','$sub_cat_id','$group_id','$services_service_id','$service_price_type_id','$service_price','$created_at')";
+	$saveItems = "INSERT INTO `services_cart`(`user_id`, `session_cart_id`, `group_id`, `service_id`, `services_price_type_id`, `service_price`, `created_at`) VALUES ('$user_id','$session_cart_id','$group_id','$services_service_id','$service_price_type_id','$service_price','$created_at')";
 	$saveCart = $conn->query($saveItems);
 	echo $getTotalCount;
-}
+} 
 
 ?>
