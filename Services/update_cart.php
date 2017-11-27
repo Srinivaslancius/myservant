0b4112c632ed7338f45363b281d186f0934b79c0
Srinivaslancius <srinivas@lanciussolutions.com>
2017-11-27 19:35:14 +0530

    new chnages

--- Services/update_cart.php
+++ Services/update_cart.php
@@ -11,11 +11,12 @@ if(isset($_POST["submit"]) && $_POST["submit"]!="") {
 		$serviceDate=date_create($_POST["service_visit_date"][$i]);
 		$getServiceDate=date_format($serviceDate,"Y-m-d");
 
-		$service_visit_time1 = date('H:i:s A', strtotime($_POST["service_visit_time"][$i]));
+		$service_visit_time1 = date('H:i:s', strtotime($_POST["service_visit_time"][$i]));
 
 		$updateq = "UPDATE services_cart SET service_selected_date = '" . $getServiceDate . "',service_selected_time ='" . $service_visit_time1 . "' WHERE id = '" . $_POST["cart_id"][$i] . "'";
-		$result = $conn->query($updateq);
+		//$result = $conn->query($updateq);
 	}
+	
 	header('Location: cart.php?suc=1');
 }
 
