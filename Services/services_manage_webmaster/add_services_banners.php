64c86b3af5ed5df37397f4dfdfc36d54de39691b
Srinivaslancius <srinivas@lanciussolutions.com>
2017-11-29 14:28:31 +0530

    new changes in sliders admin and web

--- Services/services_manage_webmaster/add_services_banners.php
+++ Services/services_manage_webmaster/add_services_banners.php
@@ -5,11 +5,17 @@ if (!isset($_POST['submit']))  {
   echo "fail";
 }else  {
   //If success
-  $lkp_banner_type_id = $_POST['lkp_banner_type_id'];
-  $service_category_id = $_POST['service_category_id'];
+  //echo "<pre>"; print_r($_POST); die;
+  $lkp_banner_type_id = $_POST['lkp_banner_type_id'];  
   $title = $_POST['title'];
   $lkp_status_id = $_POST['lkp_status_id'];
   $fileToUpload = $_FILES["fileToUpload"]["name"];
+
+  if($lkp_banner_type_id == 2) {
+    $service_category_id = "0";
+  } else {
+    $service_category_id = $_POST['service_category_id'];
+  }
   
   if($fileToUpload!='') {
 
