f526db510728550da113554c634b6b6d3ea3bb96
Srinivaslancius <srinivas@lanciussolutions.com>
2017-12-01 12:43:15 +0530

    new desing issues

--- Services/sub_categories.php
+++ Services/sub_categories.php
@@ -86,7 +86,7 @@
                  while($getAllSubCategoriesData = $getSubCategoriesData->fetch_assoc()) { ?>           
 				<div class="col-md-2 col-sm-6 wow zoomIn" data-wow-delay="0.1s">
 					<a href="list.php?key1=<?php echo encryptPassword($cat_id); ?>&key2=<?php echo $getAllSubCategoriesData['id']; ?>">
-					<div class="tour_container">
+					<div class="tour_container" style="height:180px">
 						<div class="ribbon_3 popular"><!-- <span>Popular</span> --> 
 						</div>
 						<div class="img_container padd15">
