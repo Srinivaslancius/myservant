4eaddebbd3799ea089a13b66395d2d5b393c1b86
Gunavardhan <vardhan.gujjula@gmail.com>
2017-12-05 14:33:58 +0530

    site settings chnages committed

--- Services/services_manage_webmaster/site_settings.php
+++ Services/services_manage_webmaster/site_settings.php
@@ -9,16 +9,18 @@
     $id=1;
     $admin_title = $_POST['admin_title'];  
     $email = $_POST['email'];
+    $from_email = $_POST['from_email'];
+    $orders_email = $_POST['orders_email'];
+    $contact_email = $_POST['contact_email'];
+    $google_analytics_code = $_POST['google_analytics_code'];
+    $sms_gateway_no = $_POST['sms_gateway_no'];
     $fb_link = $_POST['fb_link'];
     $twitter_link = $_POST['twitter_link'];
     $gplus_link = $_POST['gplus_link'];
     $mobile = $_POST['mobile'];    
     $footer_text = $_POST['footer_text'];
-    $service_tax = $_POST['service_tax'];
-    $open_timings = $_POST['open_timings'];
     $address = $_POST['address'];
-    $inst_link = $_POST['inst_link'];
-    $linked_link = $_POST['linked_link'];    
+    $inst_link = $_POST['inst_link'];    
 
     if($_FILES["logo"]["name"]!='') {
                                           
@@ -35,7 +37,7 @@
         $getImgUnlink = getImageUnlink('logo','services_site_settings','id',$id,$target_dir);
         //Send parameters for img val,tablename,clause,id,imgpath for image ubnlink from folder
         if (move_uploaded_file($_FILES["logo"]["tmp_name"], $target_file)) {
-            $sql = "UPDATE `services_site_settings` SET admin_title = '$admin_title', email='$email', fb_link='$fb_link', twitter_link='$twitter_link', gplus_link='$gplus_link',inst_link='$inst_link',linked_link='$linked_link', mobile='$mobile', logo = '$logo', service_tax = '$service_tax', footer_text='$footer_text', address='$address' WHERE id = '$id' ";
+            $sql = "UPDATE `services_site_settings` SET admin_title = '$admin_title', email='$email',from_email='$from_email',orders_email='$orders_email',contact_email='$contact_email',google_analytics_code='$google_analytics_code',sms_gateway_no='$sms_gateway_no', fb_link='$fb_link', twitter_link='$twitter_link', gplus_link='$gplus_link',inst_link='$inst_link', mobile='$mobile', logo = '$logo',footer_text='$footer_text', address='$address' WHERE id = '$id' ";
             if($conn->query($sql) === TRUE){
                echo "<script type='text/javascript'>window.location='site_settings.php?msg=success'</script>";
             } else {
@@ -46,7 +48,7 @@
             echo "Sorry, there was an error uploading your file.";
         }
     }  else {
-        $sql = "UPDATE `services_site_settings` SET admin_title = '$admin_title', email='$email', fb_link='$fb_link', twitter_link='$twitter_link', gplus_link='$gplus_link',inst_link='$inst_link',linked_link='$linked_link', mobile='$mobile', service_tax = '$service_tax',footer_text='$footer_text', address='$address' WHERE id = '$id' ";
+        $sql = "UPDATE `services_site_settings` SET admin_title = '$admin_title', email='$email',from_email='$from_email',orders_email='$orders_email',contact_email='$contact_email',google_analytics_code='$google_analytics_code',sms_gateway_no='$sms_gateway_no', fb_link='$fb_link', twitter_link='$twitter_link', gplus_link='$gplus_link',inst_link='$inst_link', mobile='$mobile',footer_text='$footer_text', address='$address' WHERE id = '$id' ";
         if($conn->query($sql) === TRUE){
            echo "<script type='text/javascript'>window.location='site_settings.php?msg=success'</script>";
         } else {
@@ -80,6 +82,30 @@
                     <div class="help-block with-errors"></div>
                   </div>
                   <div class="form-group">
+                    <label for="form-control-2" class="control-label">From Email</label>
+                    <input type="email" name="from_email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" class="form-control" id="form-control-2" placeholder="From Email" data-error="Please enter from email address." value="<?php echo $getSiteSettingsData['from_email'];?>" required>
+                    <div class="help-block with-errors"></div>
+                  </div>
+                  <div class="form-group">
+                    <label for="form-control-2" class="control-label">Orders Email</label>
+                    <input type="email" name="orders_email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" class="form-control" id="form-control-2" placeholder="Orders Email" data-error="Please enter orders email address." value="<?php echo $getSiteSettingsData['orders_email'];?>" required>
+                    <div class="help-block with-errors"></div>
+                  </div>
+                  <div class="form-group">
+                    <label for="form-control-2" class="control-label">Contact Email</label>
+                    <input type="email" name="contact_email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" class="form-control" id="form-control-2" placeholder="Contact Email" data-error="Please enter contact email address." value="<?php echo $getSiteSettingsData['contact_email'];?>" required>
+                    <div class="help-block with-errors"></div>
+                  </div>
+                  <div class="form-group">
+                    <label for="form-control-4" class="control-label">Google Analytics Code</label>
+                    <textarea type="text" name="google_analytics_code" class="form-control" id="form-control-2" placeholder="Google Analytics Code" data-error="This field is required." required><?php echo $getSiteSettingsData['google_analytics_code'];?></textarea>
+                  </div>
+                  <div class="form-group">
+                    <label for="form-control-2" class="control-label">Sms Gateway Number</label>
+                    <input type="text" name="sms_gateway_no" class="form-control" id="form-control-2"  placeholder="Sms Gateway Number" data-error="Please enter sms gateway number." value="<?php echo $getSiteSettingsData['sms_gateway_no'];?>" onkeypress="return isNumberKey(event)" required>
+                    <div class="help-block with-errors"></div>
+                  </div>
+                  <div class="form-group">
                     <label for="form-control-2" class="control-label">Facebook Link</label>
                     <input type="url" name="fb_link" class="form-control" id="form-control-2" placeholder="Facebook Link" data-error="Please enter a valid Facebook Link." value="<?php echo $getSiteSettingsData['fb_link'];?>" required>
                     <div class="help-block with-errors"></div>
@@ -92,7 +118,7 @@
                   </div>
 
                   <div class="form-group">
-                    <label for="form-control-2" class="control-label">Instagram Link</label>
+                    <label for="form-control-2" class="control-label">LinkedIn Link</label>
                     <input type="url" name="inst_link" class="form-control" id="form-control-2" placeholder="Instagram Link" data-error="Please enter a valid Instagram Link." value="<?php echo $getSiteSettingsData['inst_link'];?>" required>
                     <div class="help-block with-errors"></div>
                   </div>
@@ -104,14 +130,8 @@
                   </div>
 
                   <div class="form-group">
-                    <label for="form-control-2" class="control-label">Linked In Link</label>
-                    <input type="url" name="linked_link" class="form-control" id="form-control-2" placeholder="Linked In Link" data-error="Please enter a valid Linked In Link." value="<?php echo $getSiteSettingsData['linked_link'];?>" required>
-                    <div class="help-block with-errors"></div>
-                  </div>
-
-                  <div class="form-group">
                     <label for="form-control-2" class="control-label">Mobile</label>
-                    <input type="text" name="mobile" class="form-control valid_mobile_num" id="form-control-2"  placeholder="Mobile" data-error="Please enter valid Mobile." value="<?php echo $getSiteSettingsData['mobile'];?>" maxlength="10" pattern="[0-9]{10}" required>
+                    <input type="text" name="mobile" class="form-control" id="form-control-2"  placeholder="Mobile" data-error="Please enter valid Mobile." value="<?php echo $getSiteSettingsData['mobile'];?>" onkeypress="return isNumberKey(event)" maxlength="10" pattern="[0-9]{10}" required>
                     <div class="help-block with-errors"></div>
                   </div>
                   
@@ -125,12 +145,6 @@
                         Choose file...
                         <input name="logo" id="form-control-22" class="file-upload-input" type="file" multiple="multiple" onchange="loadFile(event)" accept="image/*">
                       </label>
-                  </div>
-
-                  <div class="form-group">
-                    <label for="form-control-2" class="control-label">Service Tax</label>
-                    <input type="text" name="service_tax" class="form-control valid_mobile_num" id="form-control-2" placeholder="Service Tax" data-error="Please enter valid Service Tax." value="<?php echo $getSiteSettingsData['service_tax'];?>" required>
-                    <div class="help-block with-errors"></div>
                   </div>
 
                   <div class="form-group">
