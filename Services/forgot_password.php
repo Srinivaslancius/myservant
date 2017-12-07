95e07a7eff1cab9d802842709f3a93a9469f0c30
Gunavardhan <vardhan.gujjula@gmail.com>
2017-12-07 14:40:55 +0530

    email chnages committed

--- Services/forgot_password.php
+++ Services/forgot_password.php
@@ -43,8 +43,8 @@
 			</body>';
 
 			//echo $message; die;
-            $name = "My Servant";
-			$from = "info@myservant.com";
+			$name = "My Servant";
+			$from = $getSiteSettingsData["from_email"];
 			$headers = "MIME-Version: 1.0" . "\r\n";
 			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";  
 			$headers .= 'From: '.$name.'<'.$from.'>'. "\r\n";
