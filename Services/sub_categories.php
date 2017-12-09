bace7b3f5353fc36202c0fcbac84c8270b0c3798
Srinivaslancius <srinivas@lanciussolutions.com>
2017-12-09 10:35:43 +0530

    sub categories

--- Services/sub_categories.php
+++ Services/sub_categories.php
@@ -1,153 +1,196 @@
-<!DOCTYPE html>
-<!--[if IE 8]><html class="ie ie8"> <![endif]-->
-<!--[if IE 9]><html class="ie ie9"> <![endif]-->
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<?php include_once 'meta.php';?>
-	
+<?php include_once 'top_header.php'; ?>
+<?php 
+$id = $_GET['id'];
+$getProjectsData = getDataFromTables('categories',$status=NULL,'id',$id,$activeStatus=NULL,$activeTop=NULL);
+$getProjects  = $getProjectsData->fetch_assoc();
+?>
+<!-- Favicon -->
+<link rel="shortcut icon" href="images/favicon.ico" />
 
-	<!-- Favicons-->
-	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-	<link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
-	<link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
-	<link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
-	<link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
-	
-	<!-- Google web fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Gochi+Hand|Lato:300,400|Montserrat:400,400i,700,700i" rel="stylesheet">
+<!-- bootstrap -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 
-	<!-- BASE CSS -->
-	<link href="css/base.css" rel="stylesheet">
-        <link href="site_launch/css/style.css" rel="stylesheet">
+<!--  Roboto font -->
+<link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet" />
 
-	<!-- REVOLUTION SLIDER CSS -->
-	<link href="layerslider/css/layerslider.css" rel="stylesheet">
+<!-- mega menu -->
+<link href="css/mega-menu/mega_menu.css" rel="stylesheet" type="text/css" />
 
+<!-- font-awesome -->
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+
+<!-- Flaticon -->
+<link href="css/flaticon.css" rel="stylesheet" type="text/css" />
+
+<!-- owl-carousel -->
+<link href="css/owl-carousel/owl.carousel.css" rel="stylesheet" type="text/css" />
+
+<!-- General style -->
+<link href="css/general.css" rel="stylesheet" type="text/css" />
+
+<!-- main style -->
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+<!-- Style customizer -->
+<link rel="stylesheet" type="text/css" href="#" data-style="styles" />
+<link rel="stylesheet" type="text/css" href="css/style-customizer.css" />
 
 </head>
 
 <body>
 
-	<!--[if lte IE 8]>
-    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a>.</p>
-<![endif]-->
+<!--=================================
+ preloader -->
+ 
 
-	
+<!--=================================
+header -->
 
-	<div class="layer"></div>
-	<!-- Mobile menu overlay mask -->
+<header id="header" class="clean">
+<div class="topbar dark">
+ <?php include_once 'main_header.php'; ?>
+</div>
+ 
+<!--=================================
+ mega menu -->
 
-	<!-- Header================================================== -->
-	<header>
-		<?php include_once './top_header.php';?>
-		<!-- End top line-->
+<div class="menu">  
+  <!-- menu start -->
+   <nav id="menu" class="mega-menu">
+    <!-- menu list items container -->
+    <section class="menu-list-items">
+     <div class="container"> 
+      <div class="row"> 
+       <div class="col-lg-12 col-md-12"> 
+        <!-- menu logo -->
+       <?php include_once 'menu.php'; ?>
+       </div>
+      </div>
+     </div>
+    </section>
+   </nav>
+  <!-- menu end -->
+ </div>
+</header>
 
-		<div class="container">
-                    <?php include_once './menu.php';?>
-		</div>
-		<!-- container -->
-                
-        </header>
-	<!-- End Header -->
+<!--=================================
+ header -->
 
-	<main>
-		<!-- Slider -->
-		<?php $cat_id = decryptPassword($_GET['key']); ?>
-		<div class="container-fluid page-title">
-			 <?php	$getBanners = getAllDataWhereWithActive('services_banners','service_category_id',$cat_id); 
-			 if($getBannersData = $getBanners->fetch_assoc()) { ?>
-				<div class="row">
-					<img src="<?php echo $base_url . 'uploads/services_banner_images/'.$getBannersData['banner'] ?>" class="img-responsive">
-				</div>
-			<?php } else { ?>
-				<div class="row">
-					<img src="img/slides/slide_3.jpg" class="img-responsive">
-				</div>
-			<?php } ?>
-	    </div>
 
-		<div class="container margin_60">
+<!--=================================
+ banner -->
 
-			<div class="main_title">
-				<h2>Our <span>Services</span> Sub Categories</h2>
-				
-			</div>
-			<?php if (isset($_POST['search'])) {
-				$cat_id = $_POST['id']; 
-				$getSubCategoriesData = getAllDataWhereWithActive('services_sub_category','services_category_id',$cat_id); 
-			} else {
-				$getSubCategoriesData = getAllDataWhereWithActive('services_sub_category','services_category_id',$cat_id);
-			} ?>
+<section class="inner-intro bg bg-fixed bg-overlay-black-70" style="background-image:url(<?php echo $base_url . 'uploads/category_images/'.$getProjects['category_image'] ?>);">
+  <div class="container">
+     <div class="row intro-title text-center">
+           <div class="col-sm-12">
+        <div class="section-title"><h1 class="title text-white"><?php echo $getProjects['category_name']?></h1></div>
+           </div>
+           <div class="col-sm-12">
+             <ul class="page-breadcrumb">
+                <li><a href="index.php"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-double-right"></i></li>
+                <li><a href="javascript:void(0)">Projects</a> <i class="fa fa-angle-double-right"></i></li>
+                <li><span><?php echo $getProjects['category_name']?></span> </li>
+             </ul>
+        </div>
+     </div>
+  </div>
+</section>
 
-			<div class="row">
-                 <?php if($getSubCategoriesData->num_rows > 0) { 
-                 while($getAllSubCategoriesData = $getSubCategoriesData->fetch_assoc()) { ?>           
-				<div class="col-md-2 col-sm-6 wow zoomIn" data-wow-delay="0.1s">
-					<a href="list.php?key1=<?php echo encryptPassword($cat_id); ?>&key2=<?php echo $getAllSubCategoriesData['id']; ?>">
-					<div class="tour_container" style="height:180px">
+<!--=================================
+ banner -->
 
-						<div class="ribbon_3 popular"><!-- <span>Popular</span> --> 
-						</div>
-						<div class="img_container padd15">
-                        <img src="<?php echo $base_url . 'uploads/services_sub_category_images/'.$getAllSubCategoriesData['sub_category_image'] ?>" style="width:64px; height:64px;" class="img-responsive" alt="<?php echo $getAllSubCategoriesData['sub_category_name']; ?>">
-						</div>
-						<div class="tour_title">
-							<h3><?php echo $getAllSubCategoriesData['sub_category_name']; ?></h3>
-							
-							<!-- end rating -->
-						</div>
-					</div>
-					</a>
-					<!-- End box tour -->
-				</div>
-                 <?php } } else { echo "<h3 style='text-align:center;'>Sorry! Items Not found</h3>"; } ?>
-				<!-- End col-md-4 -->
-				<!-- End col-md-4 -->
-			</div>
-			<!-- End row -->
-			
-			<hr>
-		</div>
-		<?php include_once 'our_associate_partners.php';?>
-		<!-- End section -->
-		
-	</main>
-	<!-- End main -->
 
-	<footer>
-            <?php include_once 'footer.php';?>
-    </footer><!-- End footer -->
 
-	<div id="toTop"></div><!-- Back to top button -->
-	
-	<!-- Search Menu -->
-	
-	<!-- Common scripts -->
-	<script src="../cdn-cgi/scripts/78d64697/cloudflare-static/email-decode.min.js"></script><script src="js/jquery-2.2.4.min.js"></script>
-	<script src="js/common_scripts_min.js"></script>
-	<script src="js/functions.js"></script>
+<!--=================================
+ Page Section -->
 
-	<!-- Specific scripts -->
-	<script src="layerslider/js/greensock.js"></script>
-	<script src="layerslider/js/layerslider.transitions.js"></script>
-	<script src="layerslider/js/layerslider.kreaturamedia.jquery.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function () {
-			'use strict';
-			$('#layerslider').layerSlider({
-				autoStart: true,
-				responsive: true,
-				responsiveUnder: 1280,
-				layersContainer: 1170,
-				skinsPath: 'layerslider/skins/'
-					// Please make sure that you didn't forget to add a comma to the line endings
-					// except the last line!
-			});
-		});
-	</script>
+<section class="content-box3 page-section-ptb pb-40">
+      <?php 
+      $subCat = "SELECT * FROM sub_categories WHERE category_id = '$id' AND status=0 ORDER BY id DESC";
+      $res = $conn->query($subCat);
+      $catNum = $res->num_rows;
+      if($catNum!=0) {
+        while($getSubCat = $res->fetch_assoc()){
+      ?>
+  <div class="container"><div class="row text-justify">
+  <div class="col-sm-12"><div class="section-title text-center">
+      <h2 class="title"><?php echo $getSubCat['sub_category_name']; ?></h2>
+  </div></div>
+
+   <?php /*$subCat = "SELECT * FROM sub_sub_categories WHERE sub_category_id = '".$getSubCat['id']."' AND category_id = '$id' ";
+        $res1 = $conn->query($subCat);
+      while($getSubSubCat = $res1->fetch_assoc()){*/
+  ?>
+  <!-- <div class="col-sm-12"><div class="section-title text-left">
+    <h4 class="title"><?php echo $getSubSubCat['sub_sub_category_name']; ?></h4>
+  </div></div> -->
+  <?php 
+        $getPro = "SELECT * FROM projects WHERE sub_category_id = '".$getSubCat['id']."' AND category_id = '".$id."' AND status = 0";
+        $res2 = $conn->query($getPro);
+        while($getProjects = $res2->fetch_assoc()){
+        $lid= $getProjects['location_id'];
+  ?>
+  <div class="col-sm-3">
+    <div class="about mb-40">
+          <div class="about-image clearfix">
+
+<a class="button link" href="display_project_view.php?id=<?php echo $getProjects['id'];?>&cid=<?php echo $id;?>"><img class="img-responsive" src="<?php echo $base_url . 'uploads/projects_images/'.$getProjects['images'] ?>" alt="" style="width:263px; height:185px"></a></div>
+          <div class="about-details">
+            
+            <h5 class="title"><a href="display_project_view.php?id=<?php echo $getProjects['id'];?>&cid=<?php echo $id;?>"><?php echo $getProjects['project_name'];?>, 
+              <small><?php $sql = "SELECT * FROM lkp_locations WHERE id = '$lid'";
+              $result = $conn->query($sql);
+              $row = $result->fetch_assoc(); echo $row['location_name'];?></small></a></h5>
+            <div class="about-des"><?php echo substr($getProjects['description'], 0, 250);?></div>
+             <a class="button link" href="display_project_view.php?id=<?php echo $getProjects['id'];?>&cid=<?php echo $id;?>"><span>Read More <i class="fa fa-long-arrow-right" aria-hidden="true"></i></span></a> 
+          </div>                
+    </div>
+  </div>
+  <?php }  //}?>
+  
+</div></div>
+<?php } }else { ?>
+  <div><p style="text-align:center">No Projects Found</p></div>
+<?php } ?>
+</section>
+
+<!--=================================
+footer -->
+ 
+ <footer class="footer dark-bg page-section-pt pb-0">
+  <?php include_once 'footer.php'; ?>
+ </footer>
+
+ <!--=================================
+footer -->
+
+<!--=================================
+Color Customizer --> 
+
+<div id="back-to-top"><a class="top arrow" href="#top"><i class="fa fa-chevron-up"></i></a></div>
+
+<!--=================================
+ jquery -->
+
+
+<!-- jquery  -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+
+<!-- bootstrap -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+<!-- mega-menu -->
+<script type="text/javascript" src="js/mega-menu/mega_menu.js"></script>
+
+<!-- owl-carousel -->
+<script type="text/javascript" src="js/owl-carousel/owl.carousel.min.js"></script>
+
+<!-- style customizer  -->
+<script type="text/javascript" src="js/style-customizer.js"></script>
+
+<!-- custom -->
+<script type="text/javascript" src="js/custom.js"></script>
 
 </body>
-
 </html>
