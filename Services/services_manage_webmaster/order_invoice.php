c2923fa97d77713614a0a0e35d141fc03fdeccd4
srinivaslancius <srinivas@lanciussolutions.com>
2017-12-13 15:25:29 +0530

    Changed orders page and index page

--- Services/services_manage_webmaster/order_invoice.php
+++ Services/services_manage_webmaster/order_invoice.php
@@ -125,13 +125,55 @@ $html2pdf = new HTML2PDF('P', 'A3', 'fr');
 $html2pdf->WriteHTML($content);
 $html2pdf->Output('../../uploads/generate_invoice/'.$getOrdersData1['order_sub_id'].'.pdf', 'F');
 
-// Email attachment
-
+// Email attachment to user
 $to = $getOrdersData1['email'];
 $from = $getSitesettingsDeatils['from_email'];
 $subject = "MY SERVANT ORDER INVOICE";
 
 $message = "<p>Dear ". $getOrdersData1['first_name'] . ", <br /><br />Please see the MY SERVANT Service Details attachment.</p><br /><br />Thank You<br/>MY SERVANT. ";
+$separator = md5(time());
+$eol = PHP_EOL;
+$filename = "../../uploads/generate_invoice/".$getOrdersData1['order_sub_id'].".pdf";
+$pdfdoc = $html2pdf->Output('', 'S');
+$attachment = chunk_split(base64_encode($pdfdoc));
+
+$headers = "From: " . $from . $eol;
+$headers .= "MIME-Version: 1.0" . $eol;
+$headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol . $eol;
+
+$body = '';
+
+$body .= "Content-Transfer-Encoding: 7bit" . $eol;
+$body .= "This is a MIME encoded message." . $eol; //had one more .$eol
+
+$body .= "--" . $separator . $eol;
+$body .= "Content-Type: text/html; charset=\"iso-8859-1\"" . $eol;
+$body .= "Content-Transfer-Encoding: 8bit" . $eol . $eol;
+$body .= $message . $eol;
+
+$body .= "--" . $separator . $eol;
+$body .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"" . $eol;
+$body .= "Content-Transfer-Encoding: base64" . $eol;
+$body .= "Content-Disposition: attachment" . $eol . $eol;
+$body .= $attachment . $eol;
+$body .= "--" . $separator . "--";
+
+if (mail($to, $subject, $body, $headers)) {
+
+    echo $msgsuccess = 'Mail Send Successfully'; die;
+} else {
+
+    $msgerror = 'Main not send';
+}
+
+//Email Attachment to admin
+$getAdminData = $orderStatus = getIndividualDetails('admin_users','id',$_SESSION['services_admin_user_id']);
+
+$to = $getAdminData['admin_email'];
+$from = $getSitesettingsDeatils['from_email'];
+$subject = "MY SERVANT ORDER INVOICE";
+
+$message = "<p>Dear Admin, <br /><br />Please see the MY SERVANT Service Details attachment.</p><br /><br />Thank You<br/>MY SERVANT. ";
 $separator = md5(time());
 $eol = PHP_EOL;
 $filename = "../../uploads/generate_invoice/".$getOrdersData1['order_sub_id'].".pdf";
